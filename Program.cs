0927eec617c05cabec64f0e859c2fbc9bee0fb88
nateaveryg <103460947+nateaveryg@users.noreply.github.com>
2022-09-26 22:28:51 -0400

    Update Program.cs
    altered line to say "Hello Cloud Run World! TWO"

--- Program.cs
+++ Program.cs
@@ -4,6 +4,6 @@ var app = builder.Build();
 var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
 app.Urls.Add(String.Concat("http://0.0.0.0:", port));
 
-app.MapGet("/", () => "Hello Cloud Run World!");
+app.MapGet("/", () => "Hello Cloud Run World! TWO");
 
 app.Run();
